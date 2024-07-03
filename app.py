1780dd5448d1b71836e07b5797ce68b4b22e3182
GitDerick <derickagouda@gmail.com>
2024-07-03 04:47:30 -0400

    Fichier principal streamlit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+#import snowflake.connector as sc
+import pandas as pd
+
+def main():
+    st.write("Connexion à Snowflake en Streamlit")
+
+    
+
+    
+
+if __name__ == "__main__":
+    main()
